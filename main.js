b341449021d8c5bdd5a0bb6a388f3c7c804a3cc5
Karim Toulba <karimtoulb8@gmail.com>
2024-08-22 23:40:11 +0300

    Completed the function for Dynamic Pets

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const template = document.querySelector("#single-pet-template");
+const wrapper = document.createDocumentFragment();
+
 // Fetching and printing the weather data from Weather.com
 async function start() {
   const weatherLink = await fetch(
@@ -12,12 +15,21 @@ async function start() {
 start();
 
 // Fetching Dynmic Pets from data file
-async function petData() {
+async function petsCards() {
     const petsLink = await fetch("pets.json");
     const petsJson = await petsLink.json();
     petsJson.forEach(pet => {
-        console.log(pet.species);
+        const clone = template.content.cloneNode(true);
+
+        clone.querySelector("h3").textContent = pet.name;
+        clone.querySelector("p").textContent = pet.description;
+        clone.querySelector("span").textContent = pet.birthYear;
+        clone.querySelector(".pet-image").innerHTML = `<img src="${pet.photo}" />`;
+
+        wrapper.appendChild(clone);
     });
+    document.querySelector(".pets-cards").appendChild(wrapper);
 }
 
-petData();
+petsCards();
+
